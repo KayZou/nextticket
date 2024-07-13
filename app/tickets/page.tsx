a42172f9ec00de8de3530f2faf7615f65b87968c
KayZou <elghazzalyzakaria@gmail.com>
2024-07-13 13:42:36 +0100

    filter component & prisma query

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -5,9 +5,12 @@ import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 import { Tags } from "lucide-react";
 import Pagination from "@/components/Pagination";
+import StatusFilter from "@/components/StatusFilter";
+import { Status } from "@prisma/client";
 
 interface SearchParams {
   page: string;
+  status: string;
 }
 
 export default async function Tickets({
@@ -19,12 +22,29 @@ export default async function Tickets({
 
   const page = parseInt(searchParams.page) || 1;
 
-  const ticketCount = await prisma.ticket.count();
+  const statuses = Object.values(Status);
+
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  let where = {};
+
+  if (status) {
+    where = { status };
+  } else {
+    where = {
+      NOT: [{ status: "CLOSED" as Status }],
+    };
+  }
+
+  const ticketCount = await prisma.ticket.count({ where });
 
   const tickets = await prisma.ticket.findMany({
     orderBy: { createdAt: "desc" },
     take: pageSize,
     skip: (page - 1) * pageSize,
+    where,
   });
 
   return (
@@ -36,6 +56,9 @@ export default async function Tickets({
         <Tags className="mr-2 h-4 w-4" size={22} />
         New Ticket
       </Link>
+      <div className="my-3">
+        <StatusFilter />
+      </div>
       <TicketsTable tickets={tickets} />
       <Pagination
         itemCount={ticketCount}
---
+++ components/StatusFilter.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+
+const statuses: { label: string; value?: string }[] = [
+  { label: "Open / Started" },
+  { label: "Open", value: "OPEN" },
+  { label: "Started", value: "STARTED" },
+  { label: "Closed", value: "CLOSED" },
+];
+
+export default function StatusFilter() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  return (
+    <Select
+      defaultValue={searchParams.get("status") || ""}
+      onValueChange={(status) => {
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        const query = params.size ? `?${params.toString()}` : "0";
+        router.push(`/tickets${query}`);
+      }}
+    >
+      <SelectTrigger className="w-[20vw]">
+        <SelectValue placeholder="Filter by status" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {statuses.map((status) => (
+            <SelectItem key={status.value || "0"} value={status.value || "0"}>
+              {status.label}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
